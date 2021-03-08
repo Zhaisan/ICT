16570b87ede593aa4ece30b72429cb47b9279a02
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-03-09 00:58:26 +0600

    Simple snake

---
+++ week4/Example1/Food.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example1
+{
+    class Food : GameObject
+    {
+        Random rnd = new Random();
+        public Food(char sign, ConsoleColor color) : base(sign, color)
+        {
+            Point location = new Point { X = rnd.Next(1, Game.Width), Y = rnd.Next(1, Game.Height) };
+            body.Add(location);
+            Draw();
+        }
+
+        
+
+        bool TestAppearingFood(Point p, List<Point> points)
+        {
+           foreach(Point i in points)
+            {
+                if(p.X == i.X || p.Y == i.Y)
+                {
+                    return false;
+                    break;
+                }
+            }
+            return true;
+        } 
+
+
+        
+       
+        public void Generate(List<Point> wormTest, List<Point> wallTest)
+        {
+
+
+            body.Clear();
+            Random random = new Random();
+
+            Point p = new Point { X = random.Next(0, 29), Y = random.Next(0, 29) };
+            
+            while(!TestAppearingFood(p, wormTest) || !TestAppearingFood(p, wallTest))
+            {
+                p = new Point { X = random.Next(0, 29), Y = random.Next(0, 29) };
+            }
+            body.Add(p);
+            Draw();
+                
+            
+            
+        }
+    }
+}
---
+++ week4/Example1/Game.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example1
+{
+    class Game
+    {
+        public static int Width { get { return 29; } }
+        public static int Height { get { return 29; } }
+
+        Worm w = new Worm('@', ConsoleColor.Green);
+        Food f = new Food('$', ConsoleColor.Yellow);
+        Wall wall = new Wall('#', ConsoleColor.DarkYellow, @"Levels/Level1.txt");
+
+
+
+        public bool IsRunning { get; set; }
+        public Game()
+        {
+            IsRunning = true;
+            Console.CursorVisible = false;
+            Console.SetWindowSize(Width, Width);
+            Console.SetBufferSize(Width, Width);
+        }
+
+        public void NewLevel(String level)
+        {
+            this.wall = new Wall('#', ConsoleColor.DarkYellow, @$"Levels/{level}.txt");
+            this.w = new Worm('@', ConsoleColor.Green);
+            this.f = new Food('$', ConsoleColor.Yellow);
+            
+        }
+
+        bool CheckCollisionFoodWithWorm()
+        {
+            return w.body[0].X == f.body[0].X && w.body[0].Y == f.body[0].Y;
+        }
+
+        bool CheckCollisionWallWithWorm()
+        {
+            for (int i = 0; i < wall.body.Count; i++)
+            {
+                if((wall.body[i].X == w.body[0].X && wall.body[i].Y == w.body[0].Y))
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+      
+        public void KeyPressed(ConsoleKeyInfo pressedKey)
+        {
+            switch (pressedKey.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    w.Move(0, -1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    w.Move(0, 1);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    w.Move(-1, 0);
+                    break;
+                case ConsoleKey.RightArrow:
+                    w.Move(1, 0);
+                    break;
+                case ConsoleKey.Escape:
+                    IsRunning = false;
+                    break;
+            }
+
+            
+            if (CheckCollisionFoodWithWorm())
+            {
+                w.Increase(w.body[0]);
+                f.Generate(w.body, wall.body);
+               
+                
+            }
+            if(w.body.Count == 5)
+            {
+                Console.Clear();
+                NewLevel("Level2");
+            }
+            if (CheckCollisionWallWithWorm())
+            {
+                IsRunning = false;
+            }
+        }
+    }
+}
