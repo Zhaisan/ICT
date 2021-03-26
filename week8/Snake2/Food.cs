cc3e11b7a84093f69eecf02f20a88994a40e9741
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-03-26 14:23:10 +0600

    upd

---
+++ week8/Snake2/Food.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Snake2
+{
+    class Food: GameObject
+    {
+        Random rnd = new Random();
+        public Food(char sign, ConsoleColor color) : base(sign, color)
+        {
+            Point location = new Point { X = rnd.Next(1, Game.Width), Y = rnd.Next(1, Game.Height) };
+            body.Add(location);
+            Draw();
+        }
+
+        bool TestAppearingFood(Point p, List<Point> points)
+        {
+            foreach (Point i in points)
+            {
+                if (p.X == i.X || p.Y == i.Y)
+                {
+                    return false;
+                    break;
+                }
+            }
+            return true;
+        }
+        public void Generate(List<Point> wormTest, List<Point> wallTest)
+        {
+
+
+            body.Clear();
+            Random random = new Random();
+
+            Point p = new Point { X = random.Next(0, 15), Y = random.Next(0, 15) };
+
+            while (!TestAppearingFood(p, wormTest) || !TestAppearingFood(p, wallTest))
+            {
+                p = new Point { X = random.Next(0, 15), Y = random.Next(0, 15) };
+            }
+            body.Add(p);
+            Draw();
+
+
+
+        }
+    }
+}
---
+++ week8/Snake2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Snake2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine("WELCOME TO THE SNAKE GAME");
+            Console.SetCursorPosition(10, 12);
+            Console.WriteLine("PRESS 'G' TO PLAY");
+            string key = Console.ReadLine();
+            if(key == "G")
+            {
+                Console.Clear();
+                Game game = new Game();
+                while (game.IsRunning)
+                {
+                    game.KeyPressed(Console.ReadKey());
+                }
+            }
+            
+
+            
+        }
+    }
+}
