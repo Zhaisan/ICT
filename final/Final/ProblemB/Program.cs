ea7c3be8c45723272c5ec60cbf456a6cb9de3494
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-05-24 20:44:10 +0600

    easy problems

---
+++ final/Final/ProblemB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProblemB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            String temp = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool check = false;
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (s[i] == temp[j])
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+                if (check != true)
+                {
+                    temp += s[i];
+                }
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
---
+++ final/Final/problemA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace problemA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] a = Console.ReadLine().Split();
+            String[] u = Console.ReadLine().Split();
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                if(int.Parse(u[i]) > int.Parse(a[i]))
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+                else if (int.Parse(u[i]) < int.Parse(a[i]))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
