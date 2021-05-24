20b5f5eccf9d0458cf8b5a5459f5df60b1865341
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-05-24 20:54:54 +0600

    easy problems

---
+++ final/Final/problemI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace problemI
+{
+    class Program
+    {
+        public static void smallDivisor()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= x; i++)
+            {
+                if(x % i == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            smallDivisor();
+        }
+    }
+}
